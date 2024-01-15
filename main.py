3f98c2d9561f71702a81203ef647b068845524d3
Seungkyu Ahn <seungkyua@gmail.com>
2024-01-15 11:42:32 +0900

    change directory for result.csv

--- main.py
+++ main.py
@@ -61,7 +61,6 @@ def get_tip():
 
     # TIP 물가연동채 13612U 모멘텀이 마이너스인 경우 방어 자산으로 전환 (1달은 21일)
     # 13612U 모멘텀 =((p0/p1–1)+(p0/p3–1)+(p0/p6–1)+(p0/p12–1))/4
-
     p0 = df['Close'][0]
     p1 = df['Close'][len(df) - 1 - (21 * 1)]
     p3 = df['Close'][len(df) - 1 - (21 * 3)]
@@ -75,6 +74,12 @@ def get_tip():
     tip_momentum_12sva = df['Close'][0] / np.mean(df['Close']) - 1
     tip_momentum_12sva = np.trunc(tip_momentum_12sva * 1000000) / 1000000
     tip_momentum_12sva = format(tip_momentum_12sva, '.6f')
+
+    try:
+        os.remove('tip.csv')
+        print(f"tip.csv 파일이 삭제되었습니다.")
+    except OSError as e:
+        print(f"파일 삭제 실패: {e}")
 
     return tip_momentum_13612u, tip_momentum_12sva
 
@@ -94,6 +99,13 @@ def get_bil():
     bil_momentum = bil_mean - df['Close'][0]
     bil_momentum = np.trunc(bil_momentum * 1000000) / 1000000
     bil_momentum = format(bil_momentum, '.6f')
+
+    try:
+        os.remove('bil.csv')
+        print(f"bil.csv 파일이 삭제되었습니다.")
+    except OSError as e:
+        print(f"파일 삭제 실패: {e}")
+
     return bil_momentum
 
 
@@ -112,6 +124,13 @@ def get_tlt():
     tlt_momentum = tlt_mean - df['Close'][0]
     tlt_momentum = np.trunc(tlt_momentum * 1000000) / 1000000
     tlt_momentum = format(tlt_momentum, '.6f')
+
+    try:
+        os.remove('tlt.csv')
+        print(f"tlt.csv 파일이 삭제되었습니다.")
+    except OSError as e:
+        print(f"파일 삭제 실패: {e}")
+
     return tlt_momentum
 
 
@@ -130,12 +149,23 @@ def get_pdbc():
     pdbc_momentum = pdbc_mean - df['Close'][0]
     pdbc_momentum = np.trunc(pdbc_momentum * 1000000) / 1000000
     pdbc_momentum = format(pdbc_momentum, '.6f')
+
+    try:
+        os.remove('pdbc.csv')
+        print(f"pdbc.csv 파일이 삭제되었습니다.")
+    except OSError as e:
+        print(f"파일 삭제 실패: {e}")
+
     return pdbc_momentum
 
 
 def init():
     current_directory = os.getcwd()
     print(f"현재 작업 디렉토리: {current_directory}")
+
+    file_dir = f'{current_directory}/data'
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
 
     date, snp500_value_first = get_daily_snp500_first()
     snp500_value_second = get_daily_snp500_second()
@@ -144,7 +174,7 @@ def init():
     tlt_momentum = get_tlt()
     pdbc_momentum = get_pdbc()
 
-    with open(f'{current_directory}/data/result.csv', 'a', encoding='utf-8') as file:
+    with open(f'{file_dir}/result.csv', 'a', encoding='utf-8') as file:
         file.write(f'{date},{snp500_value_first},{snp500_value_second},{tip_momentum_13612u},{tip_momentum_12sva},'
                    f'{bil_momentum},{tlt_momentum},{pdbc_momentum}\n')
 
